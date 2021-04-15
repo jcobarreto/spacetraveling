bd669bc473606cee19f835e2ae129bc9ec64abc3
jcobarreto <julio.barreto@gmail.com>
2021-04-15 09:29:07 -0300

    Challenge finalized

---
+++ src/components/Comments/index.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+
+import styles from './comments.module.scss';
+
+export default class Comments extends Component {
+  constructor(props) {
+    super(props);
+    this.commentBox = React.createRef();
+  }
+
+  componentDidMount() {
+    let scriptEl = document.createElement('script');
+    scriptEl.setAttribute('src', 'https://utteranc.es/client.js');
+    scriptEl.setAttribute('crossorigin', 'anonymous');
+    scriptEl.setAttribute('async', true);
+    scriptEl.setAttribute('repo', 'gisabernardess/spacetraveling');
+    scriptEl.setAttribute('issue-term', 'title');
+    scriptEl.setAttribute('label', 'blog-comment');
+    scriptEl.setAttribute('theme', 'github-dark');
+    this.commentBox.current.appendChild(scriptEl);
+  }
+
+  render() {
+    // eslint-disable-next-line react/jsx-filename-extension
+    return <div ref={this.commentBox} className={styles.commentBox}></div>;
+  }
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,16 @@
+import { useState } from 'react';
 import { GetStaticProps } from 'next';
+import Head from 'next/head';
+import Link from 'next/link';
+import Prismic from '@prismicio/client';
 
+import { format } from 'date-fns';
+import ptBR from 'date-fns/locale/pt-BR';
+import ApiSearchResponse from '@prismicio/client/types/ApiSearchResponse';
+import { FiCalendar, FiUser } from 'react-icons/fi';
+import Header from '../components/Header';
 import { getPrismicClient } from '../services/prismic';
+
 
 import commonStyles from '../styles/common.module.scss';
 import styles from './home.module.scss';
@@ -22,15 +32,91 @@ interface PostPagination {
 
 interface HomeProps {
   postsPagination: PostPagination;
+  preview: boolean;
 }
 
-// export default function Home() {
-//   // TODO
-// }
+export default function Home({ postsPagination, preview }: HomeProps) {
+  const [posts, setPosts] = useState<Post[]>(postsPagination.results);
+  const [hasMorePosts, setHasMorePosts] = useState(!!postsPagination.next_page);
 
-// export const getStaticProps = async () => {
-//   // const prismic = getPrismicClient();
-//   // const postsResponse = await prismic.query(TODO);
+  async function handleLoadMorePosts(): Promise<void> {
+    const loadMorePostsResponse: ApiSearchResponse = await (
+      await fetch(postsPagination.next_page)
+    ).json();
 
-//   // TODO
-// };
+    setPosts([...posts, ...loadMorePostsResponse.results]);
+    setHasMorePosts(!!loadMorePostsResponse.next_page);
+  }
+
+  return (
+    <>
+      <Head>
+        <title>Home | Space Traveling</title>
+      </Head>
+
+      <Header />
+
+      <main className={`${commonStyles.container} ${styles.content}`}>
+        <div className={`${commonStyles.container} ${styles.posts}`}>
+          {posts?.map(post => (
+            <Link key={post.uid} href={`/post/${post.uid}`}>
+              <a>
+                <strong className={styles.title}>{post.data.title}</strong>
+                <p>{post.data.subtitle}</p>
+                <div>
+                  <time>
+                    <FiCalendar />
+                    {format(
+                      new Date(post.first_publication_date),
+                      'dd MMM yyyy',
+                      {
+                        locale: ptBR,
+                      }
+                    )}
+                  </time>
+                  <span>
+                    <FiUser />
+                    {post.data.author}
+                  </span>
+                </div>
+              </a>
+            </Link>
+          ))}
+        </div>
+        {!!hasMorePosts && (
+          <button type="button" onClick={handleLoadMorePosts}>
+            Carregar mais posts
+          </button>
+        )}
+        {preview && (
+          <aside className={commonStyles.exitPreview}>
+            <Link href="/api/exit-preview">
+              <a>Sair do modo Preview</a>
+            </Link>
+          </aside>
+        )}
+      </main>
+    </>
+  );
+}
+
+export const getStaticProps = async ({ preview = false, previewData }) => {
+  const prismic = getPrismicClient();
+
+  const postsResponse = await prismic.query(
+    [Prismic.predicates.at('document.type', 'post')],
+    {
+      fetch: ['post.title', 'post.subtitle', 'post.author'],
+      pageSize: 2,
+      ref: previewData?.ref ?? null,
+    }
+  );
+
+  return {
+    props: {
+      postsPagination: postsResponse,
+      preview,
+    },
+    revalidate: 60 * 2, // 2 min
+  };
+};
